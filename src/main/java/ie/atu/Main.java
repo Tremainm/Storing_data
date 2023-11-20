d994e354e4a1bdd19edd282f22796401c3035276
Tremain <g00367973@atu.ie>
2023-11-20 09:26:52 +0000

    created "file13.txt" and used file class to identify location of file

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,28 @@
 package ie.atu;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String fileName = "File13.txt";
+
+        File myFile = new File(fileName);
+
+        try {
+            if(myFile.createNewFile())
+            {
+                System.out.println("File created successfully at " + myFile.getAbsolutePath());
+            }
+            else
+            {
+                System.out.println("File already exists at " + myFile.getAbsolutePath());
+            }
+        }
+        catch (IOException e){
+            System.out.println("An error occurred while creating the file. ");
+            e.printStackTrace();
+        }
     }
 }
