9a9978d9f11da4fe9a7ae7ca4142895cb876c77a
Tremain <g00367973@atu.ie>
2023-11-20 10:39:43 +0000

    added printwriter to format new data in file

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,7 @@ package ie.atu;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class Main {
@@ -12,19 +13,21 @@ public class Main {
         File myFile = new File(fileName);
 
 
-        try {
-            FileWriter myFile2 = new FileWriter(fileName, true);
+            try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))){
+                FileWriter myFile2 = new FileWriter(fileName, true);
 
                 System.out.println("Writing data to file.");
                 myFile2.write("after i added boolean statement.");
-                myFile2.close();
+
+                writer.println("This line will be appended");
+                writer.printf("Appended formatted content: %d %s%n", 42, "example");
 
                 System.out.println("File length: " + myFile.length());
 
+            } catch (IOException e) {
+                System.out.println("An error occurred while creating the file. ");
+                e.printStackTrace();
+            }
         }
-        catch (IOException e){
-            System.out.println("An error occurred while creating the file. ");
-            e.printStackTrace();
-        }
+
     }
-}
